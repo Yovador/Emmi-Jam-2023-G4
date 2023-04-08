1cc03c18668979a40ab3af8c1abbbd7bd049f7e7
Yoann Bouquey <ybouquey@apperture.fr>
2023-04-08 13:00:18 +0200

    (feat)[dev] Light System

--- Assets/Scripts/Installer/LevelInstaller.cs
+++ Assets/Scripts/Installer/LevelInstaller.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Zenject;
 
 public class LevelInstaller : MonoInstaller
@@ -10,5 +12,22 @@ public class LevelInstaller : MonoInstaller
     public override void InstallBindings()
     {
         Container.Bind<LevelData>().FromInstance(_levelData);
+        Container.Bind<List<ILightReceiver>>().FromInstance(LightReceivers());
+    }
+
+    //Get All the LightReceivers from the scene
+    private List<ILightReceiver> LightReceivers()
+    {
+        List<ILightReceiver> result = new List<ILightReceiver>();
+
+        List<GameObject> roots = new List<GameObject>(SceneManager.GetActiveScene().GetRootGameObjects());
+
+        IEnumerable<ILightReceiver> query = FindObjectsOfType<MonoBehaviour>().OfType<ILightReceiver>();
+        foreach (ILightReceiver child in query)
+        {
+            result.Add(child);
+        }
+        Debug.Log($"[Lgt] result {result.Count}");
+        return result;
     }
 }
---
+++ Assets/Scripts/Lights/LightBehaviour.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Zenject;
+
+[RequireComponent(typeof(Light))]
+public class LightBehaviour : MonoBehaviour
+{
+    [Inject]
+    private List<ILightReceiver> _receivers;
+    private Light _light;
+
+    [Inject]
+    private void Injection()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        CastLight();
+    }
+
+    private void CastLight()
+    {
+        foreach (ILightReceiver target in _receivers)
+        {
+            Transform targetTransform = (target as MonoBehaviour).transform;
+            Vector3 direction = Vector3.Normalize(targetTransform.position - transform.position);
+            float angle = Vector3.Angle(transform.forward, direction);
+            float demiSpotAngle = _light.spotAngle / 2;
+            Debug.Log($"[Lgt] {targetTransform.name} Angle {angle} Spotangle {demiSpotAngle}");
+            if(angle > demiSpotAngle) 
+            {
+                Debug.Log("[Lgt] Angle out of spotlight");
+                continue; 
+            }
+            CastRay(target, targetTransform, direction);
+        }
+    }
+
+    private void CastRay(ILightReceiver target, Transform targetTransform, Vector3 direction)
+    {
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit raycastHit;
+        Physics.Raycast(ray, out raycastHit, Mathf.Infinity);
+        Collider collider = raycastHit.collider;
+        ILightReceiver receiver = collider.GetComponents<Component>().OfType<ILightReceiver>().FirstOrDefault();
+        if (receiver == target)
+        {
+            receiver.ReceiveLight();
+        }
+    }
+}
