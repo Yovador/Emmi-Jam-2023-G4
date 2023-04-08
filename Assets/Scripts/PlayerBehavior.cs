896359dd1196ed649659ab6d5112dc67005e4920
Alvina DR <alvina@tavroxgames.com>
2023-04-08 12:18:48 +0200

    player basic behavior (movement and turn)

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+
+    Vector3 direction;
+    public float speed;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (direction != Vector3.zero)
+        {
+            gameObject.transform.forward = direction;
+            rb.velocity = direction * speed * Time.deltaTime;
+
+        }
+
+    }
+}
