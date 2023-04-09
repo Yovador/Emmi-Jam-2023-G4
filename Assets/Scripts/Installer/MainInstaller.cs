dd8beea31c5ec0dc41775237bb2e1c8bdfe58a42
Yoann Bouquey <ybouquey@apperture.fr>
2023-04-09 11:49:45 +0200

    (feat)[dev] SICK ASS TRANSITION

--- Assets/Scripts/Installer/MainInstaller.cs
+++ Assets/Scripts/Installer/MainInstaller.cs
@@ -3,8 +3,11 @@ using Zenject;
 
 public class MainInstaller : MonoInstaller
 {
+    [SerializeField]
+    private Animator _transitionAnimator;
     public override void InstallBindings()
     {
         Container.Bind<LevelManager>().FromComponentInHierarchy().AsSingle().NonLazy();
+        Container.Bind<Animator>().WithId("Transition").FromInstance(_transitionAnimator).AsSingle().NonLazy();
     }
 }
