0ab2de9aae85d44d0ae4a65cd57e79110d4faa4b
Merlin <gaetanjestin@outlook.com>
2023-04-22 00:20:41 +0200

    help menu and common ui rework

--- Assets/Scripts/Menu/InGameUI.cs
+++ Assets/Scripts/Menu/InGameUI.cs
@@ -2,21 +2,30 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameUI : MonoBehaviour
 {
     public CanvasGroup helpMenu;
+    public Button helpButton;
 
     public void HelpMenu()
     {
-        if (helpMenu.isActiveAndEnabled)
+        if (helpMenu.interactable)
         {
+            Time.timeScale = 1;
             helpMenu.DOFade(0, .3f);
             helpMenu.interactable = false;
+            helpButton.gameObject.SetActive(true);
+            helpMenu.blocksRaycasts = false;
         } else
         {
-            helpMenu.DOFade(1, .3f);
+            
+            helpMenu.DOFade(1, .3f).OnComplete(() => {
+                Time.timeScale = 0;
+            }) ;
             helpMenu.interactable = true;
+            helpMenu.blocksRaycasts = true;
         }
     }
 }
